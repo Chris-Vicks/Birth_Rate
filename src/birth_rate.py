3d63760ed6638a0ac3cf11ff7818e014ffccf8af
Christopher Vicks <christopher.vicks@gmail.com>
2023-07-12 17:20:36 -0400

    added .py file

--- src/birth_rate.py
+++ src/birth_rate.py
@@ -4,17 +4,23 @@ class US_Births:
     def __init__(self, file_path):
         self.file_path = file_path
         self.data = None
-        
+
+#Reads the CSV file and loads the data into the object.        
     def read_csv(self):
         self.data = pd.read_csv(self.file_path)
         print("CSV file successfully loaded.")
 
+#Removes the specified columns from the data.
+
     def remove_columns(self, columns):
         self.data.drop(columns=columns, inplace=True)
         print("Columns successfully removed.")
-        
+
+#Returns the current data.       
     def get_data(self):
         return self.data
+    
+#Filters and prints the matching states based on a keyword.
 
     def state_keyword_query(self, keyword):
         matching_states = [state for state in self.data['State'].unique() if keyword in state]
@@ -22,6 +28,9 @@ class US_Births:
         for State in matching_states:
             print(State)
 
+# Sorts the data by 'State' and 'Education Level of Mother',
+# and aggregates the 'Number of Births' by sum and 'Average Age of Mother (years)' by mean.
+# Returns the combined data.
     def sort_data(self):
         sorted_data = self.data.sort_values(by=['State', 'Education Level of Mother'])
         combined_data = sorted_data.groupby(['State', 'Year', 'Education Level of Mother']).agg({
@@ -31,7 +40,7 @@ class US_Births:
         return combined_data
 
 if __name__ == "__main__":
-    file_path = '../Data/us_births_2016_2021.csv'       
+    file_path = 'Data/us_births_2016_2021.csv'       
     data_processor = US_Births(file_path)
     data_processor.read_csv()
 
@@ -39,14 +48,14 @@ if __name__ == "__main__":
     columns_to_remove = ['State Abbreviation', 'Average Birth Weight (g)', 'Gender']  
     data_processor.remove_columns(columns_to_remove)
     
-    #creates clean DF
+    #creates clean DataFrame
     cleaned_data = data_processor.get_data()
-    cleaned_data.to_csv('../Data/Cleaned_Births.csv', index=False)
+    cleaned_data.to_csv('Data/Cleaned_Births.csv', index=False)
     print("Cleaned data saved as 'Cleaned_Births.csv'.")
 
     keyword = 'keyword'  # Replace 'keyword' with your desired keyword
     data_processor.state_keyword_query(keyword)
 
     combined_data = data_processor.sort_data()
-    combined_data.to_csv('../Data/Avg_Mothers_Birth.csv', index=False)
+    combined_data.to_csv('Data/Avg_Mothers_Birth.csv', index=False)
     print("Cleaned data saved as 'Avg_Mothers_Birth.csv'.")
